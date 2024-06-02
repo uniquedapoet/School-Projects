aa6626d022c9e964f1b05b697b849195418e8f23
Eduardo Benjamin <ebenjamin05@icloud.com>
2024-06-02 00:03:10 -0600

    Moving @ Symbol

--- Python-Projects/RougeGame/main.py
+++ Python-Projects/RougeGame/main.py
@@ -1,8 +1,14 @@
 import tcod
 
+from actions import EscapeAction, MovementAction
+from input_handler import EventHandler
+
 def main() -> None:
     screen_width = 80
     screen_height = 80
+
+    player_x = int(screen_width/2)
+    player_y = int(screen_height/2)
 
     try:
         tileset = tcod.tileset.load_tilesheet(
@@ -11,6 +17,7 @@ def main() -> None:
     except FileNotFoundError:
         print("Tileset image not found. Please check the path.")
         return
+    event_handler = EventHandler()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -20,12 +27,22 @@ def main() -> None:
         vsync=True,
     ) as context:
         root_console = tcod.console.Console(screen_width, screen_height, order="F")
+        
         while True:
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
             context.present(root_console)
 
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = event_handler.dispatch(event)
+                
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
 if __name__ == "__main__":
