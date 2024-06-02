b3962a2bda2e0fed3320b8ab61d360717b50b1f8
Eduardo Benjamin <ebenjamin05@icloud.com>
2024-06-01 22:56:01 -0600

    Making the Screen

---
+++ Python-Projects/RougeGame/main.py
@@ -0,0 +1,29 @@
+import tcod 
+
+def main() -> None:
+    screen_width = 80 
+    screen_height = 80
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="First Rouge Like Game",
+        vsync=True,
+    ) as context:
+        root_console = tcod.console(screen_width,screen_height,order="F")
+        while True:
+            root_console.print(x=1,y=1,string="@")
+
+            context.present(root_console)
+
+            for event in tcod.event.wait():
+                if event.type == "QUIT":
+                    raise SystemExit()
+
+if __name__ == "__main__":
+    main()
