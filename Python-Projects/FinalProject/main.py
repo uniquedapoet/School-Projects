25ab68795c4bcc5b50928beb43396f84a149b1ce
Eduardo Benjamin <ebenjamin05@icloud.com>
2024-06-09 04:23:09 -0600

    Refractoring Final Project

---
+++ Python-Projects/FinalProject/main.py
@@ -0,0 +1,15 @@
+"""
+Making Functions for extracting and manipulating data
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_bar_graph(df, title, x_label, y_label):
+    plt.figure(figsize=(10, 6))
+    df.plot(kind='bar', stacked=True)
+    plt.title(title)
+    plt.legend(title='Week',loc='right',bbox_to_anchor=(1.2, .5))
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    plt.show()
+    plt.savefig(title)
