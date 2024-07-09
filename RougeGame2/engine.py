f45db0689f803ff7e817a46f249d3d18e2b963ce
uniquedapoet <eddiebenjamin86@gmail.com>
2024-07-08 18:52:09 -0600

    Updated Rouge_game (CWK Class)

---
+++ RougeGame2/engine.py
@@ -0,0 +1,33 @@
+from typing import Set, Iterable, Any
+
+from tcod.context import Context
+from tcod.console import Console
+
+from Entity import Entity
+from input_handler import EventHandler
+from game_map import GameMap
+
+class Engine:
+    def __init__(
+        self, entites: Set[Entity], event_handler: EventHandler, player: Entity, game_map: GameMap
+    ):
+        self.entities = entites
+        self.event_handler = event_handler
+        self.player = player
+        self.game_map = game_map
+
+    def handle_events(self, events: Iterable[Any]) -> None:
+        for event in events:
+            action = self.event_handler.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform(self, self.player)
+
+    def render(self, console: Console, context: Context) -> None:
+        self.game_map.render(console)
+        for entity in self.entities:
+            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+        context.present(console)
+        console.clear()
