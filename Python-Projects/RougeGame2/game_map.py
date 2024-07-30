1d558286552083f3b8b7e339253bb637c78e0674
uniquedapoet <eddiebenjamin86@gmail.com>
2024-07-29 18:02:58 -0600

    creating Olympic Analysis

--- Python-Projects/RougeGame2/game_map.py
+++ Python-Projects/RougeGame2/game_map.py
@@ -5,9 +5,8 @@ import tiles_types
 class GameMap:
     def __init__(self, width: int, height:int):
         self.width, self.height = width, height
-        self.tiles = np.full((width,height), fill_value=tiles_types.floor, order='F')
+        self.tiles = np.full((width,height), fill_value=tiles_types.wall, order='F')
 
-        self.tiles[30:33,22] = tiles_types.wall
 
     def in_bounds(self, x: int, y: int) -> bool:
         """Return True if x and y are inside of the bounds of this map"""
