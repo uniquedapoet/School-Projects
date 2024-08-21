dcd7f4b3084b358ce9da6f0e101682c635ab9b10
uniquedapoet <eddiebenjamin86@gmail.com>
2024-08-21 15:24:55 -0600

    Cleanup

--- portfolio-website/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menuToggle = document.querySelector('.menu-links');
-    const icon = document.querySelector('.hamburger-icon');
-    menuToggle.classList.toggle('open');
-    icon.classList.toggle('open');
-}
